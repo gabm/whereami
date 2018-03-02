e4449d35a6a3a071a2ef2ae445ecc08f12469ccc
Matthias Gabriel <matthias.gabriel@etit.tu-chemnitz.de>
2018-03-02 10:48:11 +0100

    add cpp

--- src/whereami.h
+++ src/whereami.h
@@ -10,65 +10,81 @@ extern "C" {
 
 
 #ifndef WAI_FUNCSPEC
-#ifdef __WIN32
-    #ifdef WAI_EXPORT
-    #define WAI_FUNSPEC __declspec(dllexport)
-    #else
-    #define WAI_FUNSPEC __declspec(dllimport)
-    #endif
+#ifdef _WIN32
+#ifdef WAI_EXPORT
+#define WAI_FUNSPEC __declspec(dllexport)
 #else
-    #define WAI_FUNCSPEC
+#define WAI_FUNSPEC __declspec(dllimport)
+#endif
+#else
+#define WAI_FUNCSPEC
 #endif
 #endif
 
 #ifndef WAI_PREFIX
 #define WAI_PREFIX(function) wai_##function
 #endif
-
-/**
- * Returns the path to the current executable.
- *
- * Usage:
- *  - first call `int length = wai_getExecutablePath(NULL, 0, NULL);` to
- *    retrieve the length of the path
- *  - allocate the destination buffer with `path = (char*)malloc(length + 1);`
- *  - call `wai_getExecutablePath(path, length, NULL)` again to retrieve the
- *    path
- *  - add a terminal NUL character with `path[length] = '\0';`
- *
- * @param out destination buffer, optional
- * @param capacity destination buffer capacity
- * @param dirname_length optional recipient for the length of the dirname part
- *   of the path.
- *
- * @return the length of the executable path on success (without a terminal NUL
- * character), otherwise `-1`
- */
-WAI_FUNCSPEC
-int WAI_PREFIX(getExecutablePath)(char* out, int capacity, int* dirname_length);
-
-/**
- * Returns the path to the current module
- *
- * Usage:
- *  - first call `int length = wai_getModulePath(NULL, 0, NULL);` to retrieve
- *    the length  of the path
- *  - allocate the destination buffer with `path = (char*)malloc(length + 1);`
- *  - call `wai_getModulePath(path, length, NULL)` again to retrieve the path
- *  - add a terminal NUL character with `path[length] = '\0';`
- *
- * @param out destination buffer, optional
- * @param capacity destination buffer capacity
- * @param dirname_length optional recipient for the length of the dirname part
- *   of the path.
- *
- * @return the length of the module path on success (without a terminal NUL
- * character), otherwise `-1`
- */
-WAI_FUNCSPEC
-int WAI_PREFIX(getModulePath)(char* out, int capacity, int* dirname_length);
-
+    
+    /**
+     * Returns the path to the current executable.
+     *
+     * Usage:
+     *  - first call `int length = wai_getExecutablePath(NULL, 0, NULL);` to
+     *    retrieve the length of the path
+     *  - allocate the destination buffer with `path = (char*)malloc(length + 1);`
+     *  - call `wai_getExecutablePath(path, length, NULL)` again to retrieve the
+     *    path
+     *  - add a terminal NUL character with `path[length] = '\0';`
+     *
+     * @param out destination buffer, optional
+     * @param capacity destination buffer capacity
+     * @param dirname_length optional recipient for the length of the dirname part
+     *   of the path.
+     *
+     * @return the length of the executable path on success (without a terminal NUL
+     * character), otherwise `-1`
+     */
+    WAI_FUNCSPEC
+    int WAI_PREFIX(getExecutablePath)(char* out, int capacity, int* dirname_length);
+    
+    /**
+     * Returns the path to the current module
+     *
+     * Usage:
+     *  - first call `int length = wai_getModulePath(NULL, 0, NULL);` to retrieve
+     *    the length  of the path
+     *  - allocate the destination buffer with `path = (char*)malloc(length + 1);`
+     *  - call `wai_getModulePath(path, length, NULL)` again to retrieve the path
+     *  - add a terminal NUL character with `path[length] = '\0';`
+     *
+     * @param out destination buffer, optional
+     * @param capacity destination buffer capacity
+     * @param dirname_length optional recipient for the length of the dirname part
+     *   of the path.
+     *
+     * @return the length of the module path on success (without a terminal NUL
+     * character), otherwise `-1`
+     */
+    WAI_FUNCSPEC
+    int WAI_PREFIX(getModulePath)(char* out, int capacity, int* dirname_length);
+    
 #ifdef __cplusplus
+}
+
+#define CPP_FUNCTION(function)     inline std::string function() { \
+int length, dirname_length; \
+length = WAI_PREFIX(getExecutablePath)(NULL, 0, &dirname_length); \
+if (length <= 0) return ""; \
+    std::vector<char> path; \
+    path.resize(length + 1); \
+    WAI_PREFIX(function)(path.data(), length, &dirname_length); \
+    path[dirname_length + 1] = '\0'; \
+    return std::string(path.data()); \
+}
+
+namespace wai {
+    CPP_FUNCTION(getExecutablePath);
+    CPP_FUNCTION(getModulePath);
 }
 #endif
 
